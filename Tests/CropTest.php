860e679fb09bf9a7dece06b22afbddd71068b779
shibby <guvenatbakan@gmail.com>
2016-01-26 17:32:20 +0200

    webtest case extended from symfony webtestcase and with depends this, container methods changed

--- Tests/CropTest.php
+++ Tests/CropTest.php
@@ -2,20 +2,30 @@
 
 namespace Dugun\ImageBundle\Tests;
 
-
-use Imagine\Image\Box;
-use Imagine\Image\Point;
-use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class CropTest extends WebTestCase
 {
 
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setUp()
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $this->container = $kernel->getContainer();
+    }
+
+
     public function test_crop()
     {
 
-        $container = $this->getContainer();
-        $service = $container->get('dugun_image.service.image_service');
+        $service = $this->container->get('dugun_image.service.image_service');
 
         $file = new UploadedFile(
             __DIR__ . '/../Resources/assets/test/file1.jpg',
