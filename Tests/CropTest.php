61984731c0f30f17e0c508d4d29ccee89573420b
hayzem <aliatasever@gmail.com>
2018-04-25 17:15:14 +0300

    update composer and services based on libraries

--- Tests/CropTest.php
+++ Tests/CropTest.php
@@ -6,7 +6,7 @@ use Dugun\ImageBundle\Service\DugunImageService;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class CropTest extends \PHPUnit_Framework_TestCase
+class CropTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @var ContainerInterface
